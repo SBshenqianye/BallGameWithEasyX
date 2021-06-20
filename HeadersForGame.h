6957fb4851f426493d5254ca7f0a3d5fb7d76729
王千夜 <51922539+SBshenqianye@users.noreply.github.com>
2021-06-20 16:07:22 +0800

    Add files via upload

---
+++ HeadersForGame.h
@@ -0,0 +1,442 @@
+
+#ifndef _HEADERANDDEFS_H
+#define _HEADERANDDEFS_H
+/*开始*/
+
+/*包含了标准/非标准库函数、easyX库函数等*/
+
+/*Headers*/
+#include <stdio.h>
+#include <conio.h>
+#include <graphics.h>
+#include <stdlib.h>
+#include <time.h>
+#include <io.h>
+#include <direct.h>
+#include <windows.h>
+#include <string.h>
+
+#include <iostream>
+using namespace std;
+
+/*Defines*/
+#define OVERALL_RUNNING_SPEED 4			/*整体运行速度	-	默认为3, 3-6*/
+#define BRICK_COL_NUM 10				/*砖块列数		-	默认为10*/
+#define BRICK_DROP_TIME_NUM 30			/*砖块掉落次数	-	默认为30*/
+#define BRICK_DROP_SPEED 3				/*砖块下落速度	-	默认为1, 1-3*/
+#define BALL_INITIAL_RADIUS 8			/*小球初始半径	-	默认为8*/
+#define BALL_INITIAL_SPEED 1			/*小球初始速度	-	默认为1*/
+#define BORAD_INITIAL_LENGTH 120		/*木板初始长度	-	默认为120*/
+
+/*Global Defines*/
+const int WINDOW_HEIGHT = 600;				/*默认为600*/
+const int WINDOW_WIDTH = 800;				/*默认为800*/
+FILE *fp_debug;
+char pathname_debug[50] = ".\\bin\\debug\\";
+int count = 0;
+float score = 0;
+int numKnock = 0;
+int times = 0;
+int rank = 1;
+float DegreeofDifficulty = OVERALL_RUNNING_SPEED*1/2 * BRICK_DROP_SPEED;
+char name[21] = "\0";
+
+/*Fuction in Objects*/
+extern void saveToDebugFile(char *sentence)
+{
+	fprintf(fp_debug, sentence);
+	fclose(fp_debug);
+	if((fp_debug = fopen(pathname_debug, "a+")) == NULL)
+	{
+		printf("打开debug文件失败！\n");
+		exit(1);
+	}
+}
+
+/*Struct Defines*/
+typedef struct MyTime
+{
+	char year[5], month[3], day[3];
+	char hour[3], minute[3], second[3];
+} MyTime;
+
+typedef struct DATA
+{
+	char name[21];
+	int gamingTime;
+	int rank;
+	float score;
+	int brick_summonPos, brick_colNum, brick_dropTimeNum, brick_dropSpeed;
+	int ball_initialRadius, ball_initialSpeed;
+	int borad_initialLength, overall_runningSpeed;
+} DATA;
+
+typedef struct GAMEDATA
+{
+	DATA Data;
+	GAMEDATA *next;
+} GAMEDATA;
+
+typedef enum COLOR {yellow = 1, red, blue} COLOR;
+/*Class Defines*/
+class Brick
+{
+public:
+	int num;
+	int Pos_x, Pos_y;
+	int totalNumOfCol;
+	int Height, Width;
+	bool exist;
+	bool stopGame;
+	COLOR type;
+	Brick *next;
+	Brick(int getPos_x)
+	{
+		int temp;
+		srand(time(0));
+		temp = rand()%10;
+	//	cout << "砖块生成码为：" << temp << endl;
+		switch(temp)
+		{
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+			type = yellow;
+			break;
+		case 8:
+			type = red;
+			break;
+		case 9:
+			type = blue;
+			break;
+		default:
+			type = yellow;
+			break;
+		}
+		exist = true;					/*默认为true*/
+		stopGame = false;				/*默认为false*/
+		Pos_x = getPos_x;
+		Pos_y = 0;						/*默认为0*/
+		totalNumOfCol = BRICK_COL_NUM;
+		Height = WINDOW_HEIGHT / BRICK_DROP_TIME_NUM;
+		Width = WINDOW_WIDTH / totalNumOfCol;
+		drawBrick();
+	}
+	~Brick()
+	{
+		char num_t[10];
+		char sentence[40] = "执行Brick的析构函数(brick ";
+//		cout << "执行Brick的析构函数(brick " << num << ")\n" << endl;
+		itoa(num, num_t, 10);
+		saveToDebugFile(strcat(strcat(sentence, num_t), ")\n"));
+	}
+	void drawBrick()					/*显示砖块位置完全取决于 pos_x和 pos_y*/
+	{
+		if(exist == true)
+		{
+			if( type == blue)
+			{
+				setfillcolor(BLUE);
+				setlinecolor(BLACK);
+				fillrectangle(Pos_x*Width, Pos_y*Height, (Pos_x+1)*Width, (Pos_y+1)*Height);
+			}
+			else if(type == red)
+			{
+				setfillcolor(RED);
+				setlinecolor(BLACK);
+				fillrectangle(Pos_x*Width, Pos_y*Height, (Pos_x+1)*Width, (Pos_y+1)*Height);
+			}
+			else
+			{
+				setfillcolor(YELLOW);
+				setlinecolor(BLACK);
+				fillrectangle(Pos_x*Width, Pos_y*Height, (Pos_x+1)*Width, (Pos_y+1)*Height);
+			}
+		}
+		else
+		{
+			setfillcolor(BLACK);
+			setlinecolor(BLACK);
+			fillrectangle(Pos_x*Width, Pos_y*Height, (Pos_x+1)*Width, (Pos_y+1)*Height);
+		}
+	}
+	bool Move()
+	{
+		setfillcolor(BLACK);		/*涂黑*/
+		fillrectangle(Pos_x*Width, Pos_y*Height, (Pos_x+1)*Width, (Pos_y+1)*Height);
+
+		Pos_y += 1;
+		if(next == NULL)
+		{
+	//		cout << "system_Brick:\n\t砖块\"" << num << "\"下移一格!\n" << endl;
+		}
+		if(Pos_y >= WINDOW_HEIGHT / Height) stopGame = true;
+		drawBrick();
+		return stopGame;
+	}
+};
+
+class Borad
+{
+public:
+	int x, y;
+	int Width, Height;
+	bool Lauch;
+	Borad()
+	{
+		Width = BORAD_INITIAL_LENGTH;
+		Height = 15;				/*默认为15*/
+		Lauch = false;				/*默认为false*/
+		x = WINDOW_WIDTH/2 - Width/2;
+		y = WINDOW_HEIGHT - Height;
+		setfillcolor(BLUE);
+		solidrectangle(x, y, x + Width, y + Height);
+		drawBorad();
+	}
+	void drawBorad()
+	{
+		setfillcolor(BLUE);
+		solidrectangle(x, y, x + Width, y + Height);
+	}
+	void antidrawBorad()
+	{
+		setfillcolor(BLACK);
+		solidrectangle(x, y, x + Width, y + Height);
+	}
+	int Move()
+	{
+		int pressedKey;
+		pressedKey = _getch();
+
+		antidrawBorad();
+
+		switch(pressedKey)
+		{
+		case 75:
+		case 'a':
+		case 'A':
+			x -= Width/3;
+	//		cout << "system_Borad:\n\t木板左移一格!\n" << endl;
+			break;
+		case 77:
+		case 'd':
+		case 'D':
+			x += Width/3;
+	//		cout << "system_Borad:\n\t木板右移一格!\n" << endl;
+			break;
+		case 32:
+			if(Lauch == false)
+			{
+				Lauch = true;
+	//			cout << "system_Borad:\n\t小球发射！\n" << endl;
+				saveToDebugFile("system_Borad:\n\t小球发射！\n");
+				break;
+			}
+			else break;
+		}
+
+		if(x <= 0) x = 0;
+		if(x + Width >= WINDOW_WIDTH) x = WINDOW_WIDTH - Width;
+
+		drawBorad();
+
+		return pressedKey;
+	}
+};
+class Ball
+{
+public:
+	bool stopGame;
+	bool rebound;
+	int ball_x, ball_y, Radius;
+	int speed;
+	int ball_vx, ball_vy;
+	/*速度方向*/
+	Ball(Borad &borad)
+	{
+		stopGame = false;				/*默认为false*/
+		rebound = true;					/*默认为true*/
+		ball_vx = 1;					/*默认为1*/
+		ball_vy = -1;	/*速度方向*/	/*默认为-1*/
+		Radius = BALL_INITIAL_RADIUS;
+		speed = BALL_INITIAL_SPEED;
+		setfillcolor(RED);
+		solidcircle(ball_x, ball_y, Radius);
+	}
+	void drawBall()
+	{
+		setfillcolor(RED);
+		solidcircle(ball_x, ball_y, Radius);
+	}
+	void antidrawBall()
+	{
+		setfillcolor(BLACK);
+		solidcircle(ball_x, ball_y, Radius);
+	}
+	bool Move(Brick *brickHead, Borad &borad)
+	{
+		int x;
+		
+		if(count < 10) x = 6-OVERALL_RUNNING_SPEED;
+		else if(count < 100) x = 5-OVERALL_RUNNING_SPEED;
+		else if(count < 500) x = 4-OVERALL_RUNNING_SPEED;
+		else if(count < 1000) x = 3-OVERALL_RUNNING_SPEED;
+		if(x < 0) x = 0;
+		Sleep(x);
+		/*运动前后的绘制*/
+		BeginBatchDraw();
+		antidrawBall();
+		if(borad.Lauch == true)
+		{
+			ball_x += ball_vx * speed;
+			ball_y += ball_vy * speed;
+		}
+		else if(borad.Lauch == false)
+		{
+			ball_x = borad.x + borad.Width/2;
+			ball_y = WINDOW_HEIGHT - borad.Height - Radius - 1;
+		}
+		drawBall();
+		FlushBatchDraw();
+		/*运动前后的绘制*/
+
+		/*球与边界的交互*/
+		if(ball_x >= WINDOW_WIDTH - Radius || ball_x <= Radius)
+		{
+			/*左右边界*/
+			ball_vx *= -1;							
+//			cout << "system_Ball:\n\t小球碰到左/右边界反弹\n" << endl;
+		}
+		if(ball_y <= Radius)						
+		{
+			/*上边界*/
+			ball_vy *= -1;
+//			cout << "system_Ball:\n\t小球碰到上边界反弹\n" << endl;
+		}
+		if(ball_y + Radius >= WINDOW_HEIGHT)		
+		{
+			/*下边界*/
+//			ball_vy *= -1;
+			stopGame = true;								
+//			cout << "system_Ball:\n\t小球碰到下边界退出游戏\n" << endl;
+		}
+		/*球与边界的交互*/
+
+		/*球与木板的交互*/
+		if(borad.Lauch == true)
+		{
+			if( (ball_y + Radius == borad.y - 1) && ( ball_x + Radius >= borad.x && ball_x - Radius <= borad.x + borad.Width ) )
+			{	/*球下切等于板上端*/					/*球右切大于板左端	且	球左切小于板右端*/
+				/*木板上边缘*/
+				ball_vy *= -1;				
+//				cout << "system_Ball:\n\t小球碰到木板上边缘反弹\n" << endl;
+//				saveToDebugFile("system_Ball:\n\t小球碰到木板上边缘反弹\n");
+			}
+			else if( ball_x >= borad.x - Radius - 1 && ball_x <= borad.x && ball_y >= borad.y - Radius - 1)
+			{	/*球右切大于板左端	且	球右切小于板左端靠右*/						/*球下切大于板上端*/
+
+				/*木板左边缘*/
+				ball_vx *= -1;				
+//				cout << "system_Ball:\n\t小球碰到木板左边缘反弹\n" << endl;
+//				saveToDebugFile("system_Ball:\n\t小球碰到木板左边缘反弹\n");
+			}
+			else if( ball_x <= borad.x + borad.Width + Radius + 1 && ball_x >= borad.x + borad.Width && ball_y >= borad.y - Radius - 1)
+			{	/*球左切小于板右端	且	球左切大于板右端靠左*/											/*球下切大于板上端*/
+
+				/*木板右边缘*/
+				ball_vx *= -1;				
+//				cout << "system_Ball:\n\t小球碰到木板右边缘反弹\n" << endl;
+//				saveToDebugFile("system_Ball:\n\t小球碰到木板右边缘反弹\n");
+			}
+		}
+		/*球与木板的交互*/
+
+		/*球与砖块的交互*/
+		while(brickHead != NULL && borad.Lauch == true)
+		{
+			if( ball_x >= brickHead->Pos_x * brickHead->Width - Radius 
+				&& ball_x <= (brickHead->Pos_x + 1) * brickHead->Width + Radius 
+				&& ball_y >= brickHead->Pos_y * brickHead->Height - Radius 
+				&& ball_y <= (brickHead->Pos_y + 1) * brickHead->Height + Radius)
+				/*这一大串表示球心进入到砖块范围内*/
+			{
+				if(ball_y < brickHead->Pos_y * brickHead->Height -1)
+				{
+					/*砖块上边缘*/
+					if(rebound == true) ball_vy *= -1;
+	//				cout << "system_Ball:\n\t小球碰到砖块\"" << brickHead->num << "\"上边缘反弹" << endl;
+				}
+				else if(ball_y > (brickHead->Pos_y + 1) * brickHead->Height +1)
+				{
+					/*砖块下边缘*/
+					if(rebound == true) ball_vy *= -1;
+	//				cout << "system_Ball:\n\t小球碰到砖块\"" << brickHead->num << "\"下边缘反弹" << endl;	
+				}
+				else if(ball_x < brickHead->Pos_x * brickHead->Width -1)
+				{
+					/*砖块左边缘*/
+					if(rebound == true) ball_vx *= -1;
+	//				cout << "system_Ball:\n\t小球碰到砖块\"" << brickHead->num << "\"左边缘反弹" << endl;	
+				}
+				else if(ball_x > (brickHead->Pos_x + 1) * brickHead->Width +1)
+				{
+					/*砖块右边缘*/
+					if(rebound == true) ball_vx *= -1;		
+	//				cout << "system_Ball:\n\t小球碰到砖块\"" << brickHead->num << "\"右边缘反弹" << endl;		
+				}
+				else
+				{
+					/*砖块其他细小部分*/
+				}
+				if(brickHead->type == blue)
+				{
+					borad.Width += 60;
+				}
+				else if(brickHead->type == red)
+				{
+					ball_y += 8;
+					Radius += 8;
+				}
+				brickHead->exist = false;
+				setfillcolor(BLACK);
+				setlinecolor(BLACK);
+				fillrectangle(brickHead->Pos_x*brickHead->Width, brickHead->Pos_y*brickHead->Height, 
+					(brickHead->Pos_x+1)*brickHead->Width, (brickHead->Pos_y+1)*brickHead->Height);
+			}
+			brickHead = brickHead->next;
+		}
+		/*球与砖块的交互*/
+		return stopGame;
+	}//void Move();
+};
+
+/*Function Defines*/
+extern void Gameing();
+extern Brick *createBrick(Brick *head);
+extern bool allBrickMove(Brick *head);
+extern Brick *removeBrick(Brick *head);
+
+extern void initialization();
+extern void debugAndTest();
+extern FILE *createDebugFile();
+
+extern GAMEDATA *readFromFile();
+extern void freeMemory(GAMEDATA *head);
+extern void writeIntoFile(GAMEDATA *head);
+extern GAMEDATA *Rankthem(GAMEDATA *head);
+extern void inputName();
+
+extern void BlackScreen();
+extern void mainMenu();
+extern void mainMenuDisplay(int flag);
+extern void mainMenuControl(int *flag, int getKey);
+extern void rankMenu();
+extern void rankMenuDisplay(int flag);
+extern void rankMenuControl(int *flag, int getKey, bool *quit);
+/*结尾*/
+#endif
---
+++ main.cpp
@@ -0,0 +1,690 @@
+
+#include "HeadersForGame.h"
+#define OPTIONBAR_WIDTH WINDOW_WIDTH*3/20
+#define OPTIONBAR_HEIGHT WINDOW_HEIGHT/15
+#define OPTIONBAR_GAP WINDOW_HEIGHT/10
+#define TEXT_HEIGHT WINDOW_HEIGHT/20
+#define TEXT_GAP WINDOW_HEIGHT/40
+
+FILE *fp_data;
+GAMEDATA *HeadofData = NULL;
+
+int main()
+{
+	int system_time = 0;
+	/*准备阶段*/
+	initialization();
+	debugAndTest();
+	fp_debug = createDebugFile();
+	/*准备阶段*/
+
+	initgraph(WINDOW_WIDTH, WINDOW_HEIGHT, SHOWCONSOLE);
+	mainMenu();
+
+
+	_getch();    /*暂时还没体现出用处*/
+	fclose(fp_debug);
+	return 0;
+}
+
+void BlackScreen()
+{
+	setfillcolor(BLACK);
+	setlinecolor(BLACK);
+	fillrectangle(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+}
+
+void mainMenuDisplay(int flag)
+{
+	BeginBatchDraw();
+	LOGFONT titleFont = {0}, optionFont = {0}, tipsFont = {0};
+	titleFont.lfHeight = 120;
+	titleFont.lfWeight = 1000;
+	titleFont.lfUnderline = true;
+	optionFont.lfHeight = OPTIONBAR_HEIGHT;
+	optionFont.lfWeight = 1000;
+	tipsFont.lfHeight = OPTIONBAR_HEIGHT/2;
+	tipsFont.lfWeight = 1000;
+
+	BlackScreen();
+
+	settextstyle(&titleFont);	/*绘制标题*/
+	settextcolor(RED);
+	outtextxy(WINDOW_WIDTH*5/16, WINDOW_HEIGHT - 6 * (OPTIONBAR_HEIGHT + OPTIONBAR_GAP), _T("弹力球"));
+
+	settextstyle(&optionFont);		/*绘制选项*/
+	outtextxy(WINDOW_WIDTH/2 - OPTIONBAR_WIDTH/2, WINDOW_HEIGHT - 4 * (OPTIONBAR_HEIGHT + OPTIONBAR_GAP), 
+		_T("开始游戏"));
+	outtextxy(WINDOW_WIDTH/2 - OPTIONBAR_WIDTH/2, WINDOW_HEIGHT - 3 * (OPTIONBAR_HEIGHT + OPTIONBAR_GAP), 
+		_T("排行榜"));
+	optionFont.lfStrikeOut = true;
+	settextstyle(&optionFont);
+	outtextxy(WINDOW_WIDTH/2 - OPTIONBAR_WIDTH/2, WINDOW_HEIGHT - 2 * (OPTIONBAR_HEIGHT + OPTIONBAR_GAP), 
+		_T("设置"));
+	optionFont.lfStrikeOut = false;
+	settextstyle(&optionFont);
+	outtextxy(WINDOW_WIDTH/2 - OPTIONBAR_WIDTH/2, WINDOW_HEIGHT - OPTIONBAR_HEIGHT - OPTIONBAR_GAP, 
+		_T("退出"));
+
+	settextstyle(&tipsFont);	/*绘制提示*/
+	settextcolor(BLUE);
+	outtextxy(WINDOW_WIDTH/2 - 3*OPTIONBAR_WIDTH, WINDOW_HEIGHT - OPTIONBAR_HEIGHT - 40, 
+		_T("tips: "));
+	outtextxy(WINDOW_WIDTH/2 - 3*OPTIONBAR_WIDTH, WINDOW_HEIGHT - OPTIONBAR_HEIGHT - 20, 
+		_T("按W/S以使光标移动"));
+	outtextxy(WINDOW_WIDTH/2 - 3*OPTIONBAR_WIDTH, WINDOW_HEIGHT - OPTIONBAR_HEIGHT, 
+		_T("按回车/空格以确定"));
+
+	setlinecolor(RED);
+	setfillcolor(RED);
+	switch(flag)		/*绘制选项指针*/
+	{
+	case 3:
+		fillrectangle(WINDOW_WIDTH/2 - OPTIONBAR_WIDTH/2 - OPTIONBAR_WIDTH*2/5, 
+			WINDOW_HEIGHT - OPTIONBAR_HEIGHT - OPTIONBAR_GAP + OPTIONBAR_WIDTH/15, 
+			WINDOW_WIDTH/2 - OPTIONBAR_WIDTH/2 - OPTIONBAR_WIDTH/5, 
+			WINDOW_HEIGHT - OPTIONBAR_HEIGHT - OPTIONBAR_GAP + OPTIONBAR_WIDTH/15 + OPTIONBAR_WIDTH/5);
+		break;
+	case 4:
+		solidrectangle(WINDOW_WIDTH/2 - OPTIONBAR_WIDTH/2 - OPTIONBAR_WIDTH*2/5, 
+			WINDOW_HEIGHT - 2 * (OPTIONBAR_HEIGHT + OPTIONBAR_GAP) + OPTIONBAR_WIDTH/15, 
+			WINDOW_WIDTH/2 - OPTIONBAR_WIDTH/2 - OPTIONBAR_WIDTH/5, 
+			WINDOW_HEIGHT - 2 * (OPTIONBAR_HEIGHT + OPTIONBAR_GAP) + OPTIONBAR_WIDTH/15 + OPTIONBAR_WIDTH/5);
+		break;
+	case 2:
+		solidrectangle(WINDOW_WIDTH/2 - OPTIONBAR_WIDTH/2 - OPTIONBAR_WIDTH*2/5, 
+			WINDOW_HEIGHT - 3 * (OPTIONBAR_HEIGHT + OPTIONBAR_GAP) + OPTIONBAR_WIDTH/15, 
+			WINDOW_WIDTH/2 - OPTIONBAR_WIDTH/2 - OPTIONBAR_WIDTH/5, 
+			WINDOW_HEIGHT - 3 * (OPTIONBAR_HEIGHT + OPTIONBAR_GAP) + OPTIONBAR_WIDTH/15 + OPTIONBAR_WIDTH/5);
+		break;
+	case 1:
+		solidrectangle(WINDOW_WIDTH/2 - OPTIONBAR_WIDTH/2 - OPTIONBAR_WIDTH*2/5, 
+			WINDOW_HEIGHT - 4 * (OPTIONBAR_HEIGHT + OPTIONBAR_GAP) + OPTIONBAR_WIDTH/15, 
+			WINDOW_WIDTH/2 - OPTIONBAR_WIDTH/2 - OPTIONBAR_WIDTH/5, 
+			WINDOW_HEIGHT - 4 * (OPTIONBAR_HEIGHT + OPTIONBAR_GAP) + OPTIONBAR_WIDTH/15 + OPTIONBAR_WIDTH/5);
+		break;
+	}
+	FlushBatchDraw();
+}
+
+void mainMenuControl(int *flag, int getKey)
+{
+	switch(getKey)
+	{
+	case 'W':
+	case 'w':
+	case 72:
+		if(*flag <= 1 || *flag > 3)
+		{
+			(*flag) = 3;
+		}
+		else if(*flag > 1 || *flag <= 3)
+		{
+			(*flag)--;
+		}
+		break;
+	case 'S':
+	case 's':
+	case 80:
+		if(*flag < 1 || *flag >= 3)
+		{
+			(*flag) = 1;
+		}
+		else if(*flag >= 1 || *flag < 3)
+		{
+			(*flag)++;
+		}
+		break;
+	case 32:
+	case 13:
+		switch(*flag)
+		{
+		case 1:
+			Gameing();
+			break;
+		case 2:
+			rankMenu();
+			break;
+		case 3:
+			exit(0);
+			break;
+		case 4:
+			exit(0);
+			break;
+		}
+		break;
+	}
+}
+
+void mainMenu()
+{
+	int flag = 1, getKey;
+	for(;;)
+	{
+		mainMenuDisplay( flag);
+		getKey = _getch();
+//		cout << "getKey: " << getKey << endl;
+		mainMenuControl(&flag, getKey);
+	}
+}
+
+void rankMenuDisplay(int flag)
+{
+	GAMEDATA *item = HeadofData;
+	LOGFONT titleFont = {0}, optionFont = {0}, tipsFont = {0};
+	int i;
+	char score[10], rank[4];
+	titleFont.lfHeight = 120;
+	titleFont.lfWeight = 1000;
+	titleFont.lfUnderline = true;
+	optionFont.lfHeight = OPTIONBAR_HEIGHT;
+	optionFont.lfWeight = 1000;
+	tipsFont.lfHeight = OPTIONBAR_HEIGHT/2;
+	tipsFont.lfWeight = 1000;
+
+	BeginBatchDraw();
+	BlackScreen();
+
+	settextstyle(&titleFont);		/*标题*/
+	settextcolor(RED);
+	outtextxy(WINDOW_WIDTH*5/16, WINDOW_HEIGHT - 6 * (OPTIONBAR_HEIGHT + OPTIONBAR_GAP), _T("排行榜"));
+
+	settextstyle(&tipsFont);	/*绘制提示*/
+	settextcolor(BLUE);
+	outtextxy(WINDOW_WIDTH/2 - 3*OPTIONBAR_WIDTH, WINDOW_HEIGHT - OPTIONBAR_HEIGHT - 40, 
+		_T("tips: "));
+	outtextxy(WINDOW_WIDTH/2 - 3*OPTIONBAR_WIDTH, WINDOW_HEIGHT - OPTIONBAR_HEIGHT - 20, 
+		_T("按W/S以使页面移动"));
+	outtextxy(WINDOW_WIDTH/2 - 3*OPTIONBAR_WIDTH, WINDOW_HEIGHT - OPTIONBAR_HEIGHT, 
+		_T("按回车/空格以返回"));
+
+	settextstyle(&optionFont);
+	settextcolor(RED);
+	outtextxy(WINDOW_WIDTH*5/16 - 2*TEXT_GAP, WINDOW_HEIGHT - (TEXT_HEIGHT + TEXT_GAP)*10 - TEXT_GAP, _T("姓名"));
+	outtextxy(WINDOW_WIDTH/2 - 2*TEXT_GAP, WINDOW_HEIGHT - (TEXT_HEIGHT + TEXT_GAP)*10 - TEXT_GAP, _T("分数"));
+	outtextxy(WINDOW_WIDTH*11/16 - 2*TEXT_GAP, WINDOW_HEIGHT - (TEXT_HEIGHT + TEXT_GAP)*10 - TEXT_GAP, _T("排名"));
+	for( i = 1; i < flag && item != NULL; i++)
+	{
+		item = item->next;
+	}
+	for( i = 0; i < 10 && item != NULL; i++)
+	{
+		outtextxy(WINDOW_WIDTH*5/16 - 2*TEXT_GAP, WINDOW_HEIGHT - (TEXT_HEIGHT + TEXT_GAP)*(9 - i) - TEXT_GAP, item->Data.name);
+		itoa(item->Data.score, score, 10);
+		itoa(item->Data.rank, rank, 10);
+		outtextxy(WINDOW_WIDTH/2 - 2*TEXT_GAP, WINDOW_HEIGHT - (TEXT_HEIGHT + TEXT_GAP)*(9 - i) - TEXT_GAP, score);
+		outtextxy(WINDOW_WIDTH*11/16, WINDOW_HEIGHT - (TEXT_HEIGHT + TEXT_GAP)*(9 - i) - TEXT_GAP, rank);
+		item = item->next;
+	}
+
+	if(i < 9)
+	{
+		outtextxy(WINDOW_WIDTH*5/16, WINDOW_HEIGHT - TEXT_HEIGHT - TEXT_GAP, _T("下面没有了！"));
+	}
+
+	FlushBatchDraw();
+}
+
+void rankMenuControl(int *flag, int getKey, bool *quit)
+{
+	switch(getKey)
+	{
+	default:
+		break;
+	case 'W':
+	case 'w':
+		if(*flag > 1)
+		{
+			(*flag)--;
+		}
+		else if(*flag <= 1)
+		{
+			(*flag) = 1;
+		}
+		break;
+	case 'S':
+	case 's':
+		(*flag)++;
+		break;
+	case 32:
+	case 13:
+		*quit = true;
+		break;
+	}
+}
+
+void rankMenu()
+{
+	int flag = 1, getKey;
+	bool quit = false;
+
+//	freeMemory(HeadofData);
+	HeadofData = readFromFile();
+
+	for(;quit != true;)
+	{
+		rankMenuDisplay( flag);
+		getKey = _getch();
+//		cout << "getKey: " << getKey << endl;
+		rankMenuControl(&flag, getKey, &quit);
+	}
+}
+
+void Bin()
+{
+	setfillcolor(YELLOW);
+	solidrectangle(WINDOW_WIDTH/2 - OPTIONBAR_WIDTH/2, WINDOW_HEIGHT - (OPTIONBAR_HEIGHT + OPTIONBAR_GAP), 
+		WINDOW_WIDTH/2 + OPTIONBAR_WIDTH/2, WINDOW_HEIGHT - OPTIONBAR_GAP);
+
+	solidrectangle(WINDOW_WIDTH/2 - OPTIONBAR_WIDTH/2, WINDOW_HEIGHT - 2 * (OPTIONBAR_HEIGHT + OPTIONBAR_GAP), 
+		WINDOW_WIDTH/2 + OPTIONBAR_WIDTH/2, WINDOW_HEIGHT - (OPTIONBAR_GAP + OPTIONBAR_HEIGHT) - OPTIONBAR_GAP);
+
+	solidrectangle(WINDOW_WIDTH/2 - OPTIONBAR_WIDTH/2, WINDOW_HEIGHT - 3 * (OPTIONBAR_HEIGHT + OPTIONBAR_GAP), 
+		WINDOW_WIDTH/2 + OPTIONBAR_WIDTH/2, WINDOW_HEIGHT - 2 *(OPTIONBAR_GAP + OPTIONBAR_HEIGHT) - OPTIONBAR_GAP);
+
+	solidrectangle(WINDOW_WIDTH/2 - OPTIONBAR_WIDTH/2, WINDOW_HEIGHT - 4 * (OPTIONBAR_HEIGHT + OPTIONBAR_GAP), 
+		WINDOW_WIDTH/2 + OPTIONBAR_WIDTH/2, WINDOW_HEIGHT - 3 *(OPTIONBAR_GAP + OPTIONBAR_HEIGHT) - OPTIONBAR_GAP);
+}
+
+void debugAndTest()
+{
+
+	system("title %time%");
+
+}
+
+FILE *createDebugFile()
+{
+	FILE *fp;
+	time_t rawtime;
+	struct tm *info;
+	char buffer[30];
+	time( &rawtime );
+	info = localtime( &rawtime );
+	strftime(buffer, 30, "%Y-%m-%d-%H-%M-%S.txt", info);
+	strcat(pathname_debug, buffer);
+//	cout << pathname_debug << endl;
+	if((fp = fopen(pathname_debug, "w")) == NULL)
+	{
+		printf("创建debug文件失败！\n");
+		exit(1);
+	}
+	return fp;
+}
+
+void initialization()
+{
+	int flag;
+	FILE *fp;
+	/*创建bin文件夹*/
+	if( (flag = _access(".\\bin", 0)) == -1)
+	{
+		cout << ".\\bin\\debug文件夹不存在" << endl;
+		if((flag = mkdir(".\\bin")) == -1)
+		{
+		cout << "创建文件失败" << endl;
+		}
+		else cout << "创建文件夹成功！" << endl;
+	}
+	/*创建bin文件夹*/
+
+	/*创建debug文件夹*/
+	if( (flag = _access(".\\bin\\debug", 0)) == -1)
+	{
+		cout << ".\\bin\\debug文件夹不存在" << endl;
+		if((flag = mkdir(".\\bin\\debug")) == -1)
+		{
+		cout << "创建文件失败" << endl;
+		}
+		else cout << "创建文件夹成功！" << endl;
+	}
+	/*创建debug文件夹*/
+
+	/*创建data文件夹*/
+	if( (flag = _access(".\\bin\\data", 0)) == -1)
+	{
+		cout << ".\\bin\\data文件夹不存在" << endl;
+		if((flag = mkdir(".\\bin\\data")) == -1)
+		{
+		cout << "创建文件失败" << endl;
+		}
+		else cout << "创建文件夹成功！" << endl;
+	}
+	/*创建data文件夹*/
+
+	/*检测rank.txt是否存在*/
+	if( (fp = fopen(".\\bin\\data\\rank.txt", "r")) == NULL)
+	{
+		cout << ".\\bin\\data\\rank.txt文件不存在或无法正常打开" << endl;
+		if( (fp = fopen(".\\bin\\data\\rank.txt", "w")) == NULL)
+		{
+			cout << "创建文件失败" << endl;
+			exit(1);
+		}
+		else
+		{
+			fclose(fp);
+			if( (fp = fopen(".\\bin\\data\\rank.txt", "r")) == NULL)
+			{
+				cout << "无法打开文件.\\bin\\data\\rank.txt" << endl;
+			}
+			else fclose(fp_data);
+		}
+	}
+	else fclose(fp);
+	/*检测rank.txt是否存在*/
+}
+
+void Gameing()
+{
+	BlackScreen();
+	Brick *HeadOfBricks = NULL;
+	Borad borad;
+	Ball ball(borad);
+	bool stopGame_ball = false, stopGame_brick = false;
+	count = 0;
+	score = 0;
+	times = 0;
+
+	HeadofData = readFromFile();
+	HeadOfBricks = NULL;
+
+	srand(time(0));
+	for(;;)
+	{
+		times++;
+		stopGame_ball = ball.Move(HeadOfBricks, borad);
+		if(stopGame_ball == true)
+		{
+			cout << "你没接住小球，你失败了！" << endl;
+			break;
+		}
+		else if(stopGame_brick == true)
+		{
+			cout << "你没能清理完砖块，你失败了！" << endl;
+			break;
+		}
+		if(_kbhit())
+		{
+			borad.Move();
+		}
+		if(borad.Lauch == true)
+		{
+			if(ball.Radius > 20) ball.rebound = false;
+			else if(ball.Radius <= 20) ball.rebound = true;
+			if(times%300 == 0)
+			{
+				HeadOfBricks = createBrick(HeadOfBricks);
+			}
+			if(times%(2700/BRICK_DROP_SPEED) == 0)		/*默认为2700/BRICK_DROP_SPEED*/
+			{
+				stopGame_brick = allBrickMove(HeadOfBricks);
+			}
+			if(times%13500 == 0){		/*特殊技能失效*/
+				borad.antidrawBorad();
+				if(borad.Width > BORAD_INITIAL_LENGTH) borad.Width -= 60;
+				borad.drawBorad();
+
+				ball.antidrawBall();
+				if(ball.Radius > BALL_INITIAL_RADIUS) ball.Radius -= 8;
+				ball.drawBall();
+			}
+			HeadOfBricks = removeBrick(HeadOfBricks);
+		}
+	}/*for*/
+	
+	/*后续处理*/
+	writeIntoFile(HeadofData);
+	/*后续处理*/
+}
+
+Brick *createBrick(Brick *head)
+{
+	Brick *thisNode, *item = head;
+	int Pos_x = rand() % 10;
+	bool foundPos = false;			/*foundPos为false意味着没有给砖块找到合适的位置*/
+	if(head == NULL)
+	{
+		foundPos = true;
+	}
+	else
+	{
+	int arr[10] = {0};
+		while(item != NULL)
+		{
+			if(item->Pos_y == 0) arr[item->Pos_x] = 1;		/*值为1表明该位置有砖块*/
+			item = item->next;
+		}
+		if(arr[Pos_x] == 0) foundPos = true;
+	}
+	if(foundPos == true)
+	{
+		thisNode = new Brick(Pos_x);
+		thisNode->next = NULL;
+		thisNode->num = count;
+		count++;
+		if(head == NULL)
+		{
+			head = thisNode;
+		}
+		else
+		{
+			item = head;
+			while(item->next != NULL)		/*此循环用于将item指向链表尾端*/
+			{
+				item = item->next;
+			}
+			item->next = thisNode;
+		}
+	}
+	return head;
+}
+
+bool allBrickMove(Brick *head)
+{
+	bool stopGame = false;
+	while(head != NULL)
+	{
+		if((stopGame = head->Move()) == true) break;
+		head = head->next;
+	}
+	return stopGame;
+}
+
+Brick *removeBrick(Brick *head)
+{
+	Brick *temp;
+	Brick *lastItem;
+	Brick *item;
+
+	item = head;
+	while(item != NULL)
+	{
+		temp = item->next;
+		if(item->exist == false)
+		{
+			score++;
+			if(item == head)
+			{						/*要删的是头*/
+				
+//				cout << "head(1): 0x" << head;
+				head = head->next;
+//				cout << "head(2): 0x" << head;
+				delete item;
+				break;
+			}
+			else if(item->next == NULL)
+			{						/*要删的是尾巴*/
+				lastItem->next = NULL;
+				delete item;
+				break;
+			}
+			else 
+			{						/*要删的是身体*/
+				lastItem->next = item->next;
+				delete item;
+				break;
+			}
+//			cout << "head(3): 0x" << head;
+		}	
+		lastItem = item;
+		item = temp; 
+//		cout << "head(4): 0x" << head;
+	}
+//	cout << "head(5): 0x" << head;
+	return head;
+}
+
+GAMEDATA *readFromFile()
+{
+	GAMEDATA *head = NULL;
+	GAMEDATA *gamedata, *thisNode, *lastNode;
+	gamedata = new GAMEDATA;
+	if( (fp_data = fopen(".\\bin\\data\\rank.txt", "r")) == NULL)
+	{
+		cout << "打开文件失败（读的方式）" << endl;
+		exit(1);
+	}
+	while(!feof(fp_data))
+	{
+		if(fscanf(fp_data, "%s %f %d", gamedata->Data.name, 
+			&gamedata->Data.score, &gamedata->Data.rank) == 3)
+		{
+			thisNode = new GAMEDATA;
+			*thisNode = *gamedata;
+
+				/*
+				cout << "读到的数据: \n\t"
+				<< " " << gamedata->Data.name 
+				<< " " << gamedata->Data.score 
+				<< " " << gamedata->Data.rank << endl;
+				*/
+			thisNode->next = NULL;
+			if(head == NULL)
+			{
+				head = thisNode;
+			}
+			else
+			{
+				lastNode->next = thisNode;
+			}
+			lastNode = thisNode;
+		}
+	}
+	delete gamedata;
+	fclose(fp_data);
+	head = Rankthem(head);
+	return head;
+}
+
+void freeMemory(GAMEDATA *head)
+{
+	GAMEDATA *item = head, *temp = head->next;
+	if(head == NULL)
+	{
+		cout << "直接return" << endl;
+		return;
+	}
+	else
+	{
+		while(item != NULL)
+		{
+			cout << "item: " << item << endl;
+			temp = item->next;
+			delete item;
+			item = temp;
+		}
+	}
+}
+void writeIntoFile(GAMEDATA *head)
+{
+	GAMEDATA *gamedata, *lastNode, *item;
+	lastNode = head;
+	gamedata = new GAMEDATA;
+
+	if( (fp_data = fopen(".\\bin\\data\\rank.txt", "w")) == NULL)
+	{
+		cout << "打开文件失败（写的方式）" << endl;
+		exit(1);
+	}
+	cout << "请输入你的名字：" << endl;
+	cin >> name;
+	saveToDebugFile("名字为：");
+	saveToDebugFile(name);
+	
+	system("cls");
+
+	if(head == NULL)
+	{
+//		cout << "姓名\t" << "分数\t" << "排名" << endl;
+//		printf("%s\t%.2f\t%d\n", name, score * DegreeofDifficulty, rank);
+		fprintf(fp_data, "%s %f %d", name, score * DegreeofDifficulty, rank);
+		delete gamedata;
+	}
+	else
+	{
+		while(lastNode->next != NULL)
+		{
+			lastNode = lastNode->next;
+		}
+		strcpy(gamedata->Data.name, name);
+		gamedata->Data.score = score * DegreeofDifficulty;
+		gamedata->next = NULL;
+		lastNode->next = gamedata;
+//		cout << "姓名\t" << "分数\t" << "排名" << endl;
+		item = Rankthem(head);
+		while(item != NULL)
+		{
+//			printf("%s\t%4.6f\t%4d\n", item->Data.name, item->Data.score, item->Data.rank);
+			fprintf(fp_data, "%s %f %d\n", item->Data.name, item->Data.score, item->Data.rank);
+			item = item->next;
+		}
+	}
+	fclose(fp_data);
+}
+
+GAMEDATA *Rankthem(GAMEDATA *head)
+{
+	GAMEDATA *thisNode, *nextNode;
+	DATA temp;
+	int i = 1;
+
+	thisNode = head;
+	while(thisNode != NULL)
+	{
+		nextNode = thisNode->next;
+		while(nextNode != NULL)
+		{
+			if(nextNode->Data.score > thisNode->Data.score)
+			{
+				temp = nextNode->Data;
+				nextNode->Data = thisNode->Data;
+				thisNode->Data = temp;
+			}
+			else if(nextNode->Data.score < thisNode->Data.score)
+			{
+				;
+			}
+			else if(nextNode->Data.score == thisNode->Data.score)
+			{
+				if(strcmp(nextNode->Data.name, thisNode->Data.name) < 0)
+				{
+					temp = nextNode->Data;
+					nextNode->Data = thisNode->Data;
+					thisNode->Data = temp;
+				}
+			}
+			nextNode = nextNode->next;
+		}
+		thisNode = thisNode->next;
+	}	/*按分数、姓名排序*/
+
+	thisNode = head;
+	while(thisNode != NULL)
+	{
+		thisNode->Data.rank = i;
+		i++;
+		thisNode = thisNode->next;
+	}		/*赋序*/
+	return head;
+}
